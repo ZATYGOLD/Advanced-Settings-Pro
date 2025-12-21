440ead127bfb4fbea79e9f749cde461be018fda3
ZATYGOLD <30324251+ZATYGOLD@users.noreply.github.com>
2025-12-21 23:25:02 +0800

    Reorganized sorting index for unit movement settings

--- config/unit_movement/SettlerParameters.sql
+++ config/unit_movement/SettlerParameters.sql
@@ -9,7 +9,7 @@ INSERT OR IGNORE INTO ParameterGroups (GroupId, Name)
 -- Left Side | Setting Title Information
 INSERT OR IGNORE INTO Parameters (ParameterID, Name, Description, Domain, Hash, DefaultValue, ConfigurationGroup, ConfigurationKey,	GroupId, ChangeableAfterGameStart, SortIndex) 
     VALUES ("ZG_SettlerMovementSpeed", "LOC_ZG_SETTLER_MOVEMENT_SPEED_NAME", "LOC_ZG_SETTLER_MOVEMENT_SPEED_DESCRIPTION", 
-            "ZG_SettlerMovementDomain", 0, "ZG_SETTLER_MOVES_DEFAULT", "Game","SettlerMovementKey" , "ZGOptions", 0, 100);
+            "ZG_SettlerMovementDomain", 0, "ZG_SETTLER_MOVES_DEFAULT", "Game","SettlerMovementKey" , "ZGOptions", 0, 1050);
 
 -- Right Side | Setting Options, Description, and Additional Information
 INSERT OR IGNORE INTO DomainValues (Domain, Value, Name, Description, SortIndex)
