bff783617f95e0ac1ca03438e83cb2551236a62a
ZATYGOLD <30324251+ZATYGOLD@users.noreply.github.com>
2025-12-22 19:04:45 +0800

    Unit Movement MP override

--- config/unit_movement/TreasureConvoyParameters.sql
+++ config/unit_movement/TreasureConvoyParameters.sql
@@ -3,9 +3,9 @@
 
 -- Treasure Convoy Movement Settings
 -- Left Side | Setting Title Information
-INSERT OR IGNORE INTO Parameters (ParameterID, Name, Description, Domain, Hash, DefaultValue, ConfigurationGroup, ConfigurationKey,	GroupId, ChangeableAfterGameStart, SortIndex) 
+INSERT OR IGNORE INTO Parameters (ParameterID, Name, Description, Domain, DefaultValue, Hash, ConfigurationGroup, ConfigurationKey,	GroupId, GroupIDMultiplayerOverride, ChangeableAfterGameStart, SortIndex) 
     VALUES ("ZG_TreasureMovementSpeed", "LOC_ZG_TREASURE_MOVEMENT_SPEED_NAME", "LOC_ZG_TREASURE_MOVEMENT_SPEED_DESCRIPTION", 
-            "ZG_TreasureMovementDomain", 0, "ZG_TREASURE_MOVES_DEFAULT", "Game","TreasureMovementKey" , "ZGOptions", 0, 1051);
+            "ZG_TreasureMovementDomain", "ZG_TREASURE_MOVES_DEFAULT", 1, "Game","TreasureMovementKey", "UnitMovementOptions", "MPAdvancedUnitMovementOptions", 0, 1051);
 
 -- Right Side | Setting Options, Description, and Additional Information
 INSERT OR IGNORE INTO DomainValues (Domain, Value, Name, Description, SortIndex)
