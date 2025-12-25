61ce280ac25592388ff6c2b6f3bb1ee75a5fe74e
ZATYGOLD <30324251+ZATYGOLD@users.noreply.github.com>
2025-12-25 21:47:27 +0800

    MP Change mementos always on (for now)

--- ui/mp-change-mementos.js
+++ ui/mp-change-mementos.js
@@ -1,8 +1,5 @@
 import { i } from "/core/ui/shell/mp-staging/model-mp-staging-new.chunk.js";
 
-//const descriptor = Object.getOwnPropertyDescriptor(i, 'canEditMementos');
-//const originalFunction = descriptor?.get?.bind(i);
-
 Object.defineProperty(i, 'canEditMementos', {
     configurable: true,
     enumerable: true,
