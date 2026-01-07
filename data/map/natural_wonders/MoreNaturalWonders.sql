2f943051535178b7defe0abf46141dc75fa28ac6
ZATYGOLD <30324251+ZATYGOLD@users.noreply.github.com>
2026-01-07 21:27:19 +0800

    Updated for natural wonders to be percentage based

--- data/map/natural_wonders/MoreNaturalWonders.sql
+++ data/map/natural_wonders/MoreNaturalWonders.sql
@@ -1,5 +1,5 @@
 -- data/map/natural_wonders/MoreNaturalWonders.sql
 -- Author: Zatygold
 
-UPDATE Maps 
-SET NumNaturalWonders = NumNaturalWonders + 2;
+UPDATE Maps
+SET NumNaturalWonders = MAX(1, CAST(CEIL(COALESCE(NumNaturalWonders, 0) * 1.5) AS INTEGER));
