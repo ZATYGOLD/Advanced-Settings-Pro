37db03e2e666065a1875545dd903c5371424a192
ZATYGOLD <30324251+ZATYGOLD@users.noreply.github.com>
2026-01-08 18:46:36 +0800

    reset the oceanwidth data

--- data/map/ocean_width/MoreOceanWidth.sql
+++ data/map/ocean_width/MoreOceanWidth.sql
@@ -4,8 +4,4 @@
 -- Tiny, Small, Standard: 5
 -- Large, Huge: 10
 UPDATE Maps
-SET OceanWidth =
-  CASE
-    WHEN OceanWidth <= 0 THEN OceanWidth
-    ELSE MAX(3, CAST(ROUND(OceanWidth * 1.25) AS INTEGER))
-  END;
+SET OceanWidth = OceanWidth * 1.25;
